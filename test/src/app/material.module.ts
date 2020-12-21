b96e45255b1d701bd544b5c6cf065dcc7a9e1ce0
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-21 16:07:13 +0700

    step 13

--- test/src/app/material.module.ts
+++ test/src/app/material.module.ts
@@ -3,11 +3,15 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]  
+    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
+        MatDatepickerModule, MatNativeDateModule],
+    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
+        MatDatepickerModule, MatNativeDateModule]  
 })
 
 export class MaterialModule {}
