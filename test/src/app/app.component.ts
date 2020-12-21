01b7db686fb1a78459e4cfe87e66eeb7cbdaf0f1
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-21 17:29:25 +0700

    20

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
-  openSidenav = false;
+  @ViewChild('sidenav')
+  mySidenav;
+
+  onToggle(){
+    this.mySidenav.toggle();
+  }
 }
--- test/src/app/material.module.ts
+++ test/src/app/material.module.ts
@@ -7,15 +7,16 @@ import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
     imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-        MatSidenavModule],
+        MatSidenavModule, MatToolbarModule],
     exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-        MatSidenavModule]  
+        MatSidenavModule, MatToolbarModule]  
 })
 
 export class MaterialModule {}
