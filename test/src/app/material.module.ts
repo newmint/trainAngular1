b395a8d2b848c03ccbb570dd97e0c2c99d75544d
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-21 16:33:49 +0700

    15

--- test/src/app/material.module.ts
+++ test/src/app/material.module.ts
@@ -5,13 +5,14 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
 @NgModule({
     imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
-        MatDatepickerModule, MatNativeDateModule],
+        MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
     exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, 
-        MatDatepickerModule, MatNativeDateModule]  
+        MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]  
 })
 
 export class MaterialModule {}
