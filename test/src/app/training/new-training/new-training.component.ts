8508aeec0f4165ff31d25f9a9c2beb94759b0292
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-23 18:02:53 +0700

    40

--- test/src/app/training/new-training/new-training.component.ts
+++ test/src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-new-training',
@@ -10,9 +12,13 @@ export class NewTrainingComponent implements OnInit {
   @Output()
   trainingStart = new EventEmitter<void>();
 
-  constructor() { }
+  exercises: Exercise[] = [];
+
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit(): void {
+
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
   onStartTraining() {
