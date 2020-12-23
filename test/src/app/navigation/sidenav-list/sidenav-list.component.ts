2288a96b02794da9450cec50cebdd0151a7df32b
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-23 16:55:23 +0700

    36

--- test/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ test/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,14 +9,27 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class SidenavListComponent implements OnInit {
 
+  isAuth = false;
+
   @Output()
   sidenavClose = new EventEmitter<void>();
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
+
   onSidenavClose() {
     this.sidenavClose.emit();
   }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+
+
 }
