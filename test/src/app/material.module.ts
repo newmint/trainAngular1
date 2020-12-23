08c8c606775f43301d9945fe13755dbc7bdc8cb8
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-23 13:46:17 +0700

    26

--- test/src/app/material.module.ts
+++ test/src/app/material.module.ts
@@ -9,15 +9,16 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
 
 
 @NgModule({
     imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-        MatSidenavModule, MatToolbarModule, MatListModule],
+        MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule],
     exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule,
         MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-        MatSidenavModule, MatToolbarModule, MatListModule]
+        MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule]
 })
 
 export class MaterialModule {}
