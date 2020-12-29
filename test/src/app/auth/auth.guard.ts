fe59c1316501212496279be3b4600584c62ee843
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-29 10:23:06 +0700

    71

--- test/src/app/auth/auth.guard.ts
+++ test/src/app/auth/auth.guard.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { RouterStateSnapshot, ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot, CanActivate, Router, CanLoad, Route } from '@angular/router';
 import { AuthService } from './auth.service';
 
 //ตามความเข้าใจคือ injectable ให้ตัวอื่นมา แจมได้
 //ในที่นี้อยากใช้ authService เลยต้องให้มันถูก inject
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
     constructor(private authService: AuthService,
                 private router: Router) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if(this.authService.isAuth()) {
+            return true;
+        }else{
+            this.router.navigate(['/login']);
+        }
+    }
+
+    canLoad(route: Route) {
         if(this.authService.isAuth()) {
             return true;
         }else{
