2f942c8bf69168e1e8cc37aac2b21da6da59e746
LAPTOP-7AGM6I0V\Anan <anand@pccth.com>
2020-12-30 11:15:59 +0700

    81

--- test/src/environments/environment.prod.ts
+++ test/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDzpHtAAJeX8v9lamYwLF4nhuuOi0SpIXU",
+    authDomain: "test-ng-d5858.firebaseapp.com",
+    databaseURL: "https://test-ng-d5858-default-rtdb.firebaseio.com",
+    projectId: "test-ng-d5858",
+    storageBucket: "test-ng-d5858.appspot.com",
+    messagingSenderId: "663258469749",
+    appId: "1:663258469749:web:68c50f7d9aa300e363ef3c"
+  }
 };
